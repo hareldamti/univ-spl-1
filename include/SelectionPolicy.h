d80022751032e1d4ff1696714cf75e4da7985542
hareldamti <94783416+hareldamti@users.noreply.github.com>
2022-11-15 19:44:47 +0200

    skeleton files

---
+++ include/SelectionPolicy.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class SelectionPolicy {
+    public:
+        
+
+};
+
+class MandatesSelectionPolicy: public SelectionPolicy{ };
+
+class EdgeWeightSelectionPolicy: public SelectionPolicy{ };
---
+++ src/SelectionPolicy.cpp
@@ -0,0 +1,8 @@
+#include "SelectionPolicy.h"
+
+#
+choose(agent)
+    agent.party.neighbors -> what to take
+
+choose(party)
+    party.requests -> sort
